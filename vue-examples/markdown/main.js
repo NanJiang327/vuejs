5eb1305305b8fec38492a7cf7eaf88fe64a3b175
NanJiang327 <nj499521010@gmail.com>
2018-09-17 21:23:23 +1200

    finish git-commit page

--- vue-examples/markdown/main.js
+++ vue-examples/markdown/main.js
@@ -8,5 +8,10 @@ var vm = new Vue({
       return marked(this.input, {sanitize: true})
     }
   },
+  methods: {
+    update: _.debounce(function (e) {
+      this.input = e.target.value
+    }, 300)
+  }
   
 })
