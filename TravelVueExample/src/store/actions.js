73d21c8c971c6125e38fc865ef319579b335ae69
NanJiang327 <nj499521010@gmail.com>
2018-09-19 21:20:44 +1200

    Qunar Example

--- TravelVueExample/src/store/actions.js
+++
@@ -1,6 +0,0 @@
-export default {
-  changeCity (ctx, city) {
-    // 使用action的commit激活mutations中的changeCity事件
-    ctx.commit('changeCity', city)
-  }
-}
--- TravelVueExample/src/store/mutations.js
+++
@@ -1,9 +0,0 @@
-export default {
-  // 通过mutations的changeCity把数据mutate给state
-  changeCity (state, city) {
-    state.city = city
-    try {
-      localStorage.city = city
-    } catch (e) {}
-  }
-}
